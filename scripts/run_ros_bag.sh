51b227e25fa0248c839534b454bd4f37764c0d23
longpth <longpth.vn@gmail.com>
2020-07-12 18:04:57 +0700

    First commit

---
+++ scripts/run_ros_bag.sh
@@ -0,0 +1,2 @@
+rosbag play /media/long/OS/work/060.SLAM/rgbd_dataset_freiburg1_xyz.bag /camera/rgb/image_color:=/camera/rgb/image_raw /camera/depth/image:=/camera/depth_registered/image_raw /camera/rgb/camera_info:=/rgb/camera_info -r 0.5
+
